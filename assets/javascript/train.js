e50e5b5f6914ffcb3d18753101f67dc88156f9e3
Jennifer Zimmerman <jennifer.faye0101@gmail.com>
2019-08-09 18:47:46 -0500

    add firebase info

---
+++ assets/javascript/train.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDZsBWR5VtVg7GupeU-1yWK_M9m6jrnK9Y",
+    authDomain: "jenf-train-scheduler.firebaseapp.com",
+    databaseURL: "https://jenf-train-scheduler.firebaseio.com",
+    projectId: "jenf-train-scheduler",
+    storageBucket: "",
+    messagingSenderId: "54508970508",
+    appId: "1:54508970508:web:cb015529c9c35217"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  
